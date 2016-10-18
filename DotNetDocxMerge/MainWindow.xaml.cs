521e07a54f8add7cdfa2f4d8d66c76660992b99c
Sunny <sunnytam0401@gmail.com>
2016-10-18 16:31:02 +0800

    add a branch of function for buttons for open file dialog

--- DotNetDocxMerge/MainWindow.xaml.cs
+++ DotNetDocxMerge/MainWindow.xaml.cs
@@ -24,5 +24,69 @@ namespace DotNetDocxMerge
         {
             InitializeComponent();
         }
+
+        private void btnTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            txtTemplate.Text = OpenSelectFileDialog();
+        }
+
+        private void btnCsv_Click(object sender, RoutedEventArgs e)
+        {
+            txtCsv.Text = OpenSelectFileDialog();
+        }
+
+        #region libs
+
+        private string OpenSelectFileDialog()
+        {
+            string fileName = "";
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".docx"; // Default file extension
+            dlg.Filter = "Word document|*.docx"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                fileName = dlg.FileName;
+            }
+
+            return fileName;
+        }
+
+        private void btnDist_Click(object sender, RoutedEventArgs e)
+        {
+            txtDist.Text = OpenSaveFileDialog();
+        }
+
+        private string OpenSaveFileDialog()
+        {
+            string fileName = "";
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".docx"; // Default file extension
+            dlg.Filter = "Word document|*.docx"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                fileName = dlg.FileName;
+            }
+
+            return fileName;
+        }
+
+        #endregion
+
+        
     }
 }
